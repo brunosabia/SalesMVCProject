48e92e12ea601c1dabe8ca7eeb3844054d857cf8
Bruno Sabia <brunosabia1@gmail.com>
2020-10-08 16:52:32 -0300

    Updating the project

--- SalesWebMVC/Program.cs
+++ SalesWebMVC/Program.cs
@@ -19,6 +19,7 @@ namespace SalesWebMVC
 
         }
 
+
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
