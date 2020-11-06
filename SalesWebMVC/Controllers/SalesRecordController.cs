dcf885cdae46ad62524b159ce17ac1341af42e2f
Bruno Sabia <brunosabia1@gmail.com>
2020-11-06 15:56:03 -0300

    Preparing sales search navigation views

---
+++ SalesWebMVC/Controllers/SalesRecordController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWebMVC.Controllers
+{
+    public class SalesRecordController : Controller
+    {
+
+        //index da pagina de pesquisa
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //pesquisa simples
+        public IActionResult SimpleSearch()
+        {
+            return View();
+        }
+        //pesquisa de grupo
+        public IActionResult GroupingSearch()
+        {
+            return View();
+        }
+    }
+}
